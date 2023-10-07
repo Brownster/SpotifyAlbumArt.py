30a15eafadd74a9527bc7865df54066c22e1d0c3
Marc Brown <marcsmobile.brown@googlemail.com>
2023-10-07 23:26:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,51 +1,79 @@
 #!/bin/bash
 
-# Update and upgrade the system
-sudo apt update && sudo apt upgrade -y
+echo "Spotify Album Art Display Installation Script"
 
-# Install Python3 and PIP
-sudo apt install -y python3 python3-pip
-
-# Clone the GitHub repository
-git clone https://github.com/Brownster/SpotifyAlbumArt.py.git
-cd SpotifyAlbumArt.py
-
-# Install Python dependencies
-pip3 install -r requirements.txt
-
-# Prompt for output type
-read -p "Do you want to use HDMI or LED Matrix for album art display? (hdmi/led): " output_type
-
-# Install the RGB Matrix Library if LED Matrix is selected
-if [ "$output_type" == "led" ]; then
-    cd ~
-    git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
-    cd rpi-rgb-led-matrix
-    make
-    cd bindings/python
-    make build-python
-    sudo make install-python
-
-    # Permissions to run the script without needing root
-    cd ~/rpi-rgb-led-matrix/utils
-    sudo ./install-service.sh
-fi
-
-# Prompt for Spotify API credentials
-read -p "Enter your Spotify client_id: " client_id
-read -p "Enter your Spotify client_secret: " client_secret
-
-# Replace placeholders in the selected Python script with actual Spotify credentials
-cd ~/SpotifyAlbumArt.py
-if [ "$output_type" == "led" ]; then
-    sed -i "s/XXXXXXXXXXXXXXXXXXXXXX/$client_id/g" LED_SpotifyAlbumArt.py
-    sed -i "s/XXXXXXXXXXXXXXXXXXXXXXXXX/$client_secret/g" LED_SpotifyAlbumArt.py
-    echo "Installation completed. You can now run the LED_SpotifyAlbumArt.py script."
-elif [ "$output_type" == "hdmi" ]; then
-    sed -i "s/XXXXXXXXXXXXXXXXXXXXXX/$client_id/g" HDMI_SpotifyAlbumArt.py
-    sed -i "s/XXXXXXXXXXXXXXXXXXXXXXXXX/$client_secret/g" HDMI_SpotifyAlbumArt.py
-    echo "Installation completed. You can now run the HDMI_SpotifyAlbumArt.py script."
-else
-    echo "Invalid option selected. Please run the script again and select either 'hdmi' or 'led'."
+# Check if script is run as root
+if [ "$(whoami)" != "root" ]; then
+    echo "Please run this script as root."
     exit 1
 fi
+
+# Update and upgrade the system
+echo "Updating and upgrading the system..."
+apt-get update -y && apt-get upgrade -y
+if [ $? -ne 0 ]; then
+    echo "Failed to update and upgrade the system. Please check your network connection."
+    exit 1
+fi
+
+# Install dependencies
+echo "Installing dependencies..."
+apt-get install -y python3-dev python3-pillow libgraphicsmagick++-dev libwebp-dev
+if [ $? -ne 0 ]; then
+    echo "Failed to install dependencies."
+    exit 1
+fi
+
+# Clone the GitHub repository
+echo "Cloning the SpotifyAlbumArt repository..."
+cd /home/pi || exit
+git clone https://github.com/Brownster/SpotifyAlbumArt.py.git
+if [ $? -ne 0 ]; then
+    echo "Failed to clone the repository. Please check your network connection."
+    exit 1
+fi
+
+# Install Python requirements
+echo "Installing Python requirements..."
+pip3 install -r SpotifyAlbumArt.py/requirements.txt
+if [ $? -ne 0 ]; then
+    echo "Failed to install Python requirements."
+    exit 1
+fi
+
+# Ask for Spotify API credentials
+read -p "Enter your Spotify Client ID: " client_id
+read -p "Enter your Spotify Client Secret: " client_secret
+
+# Update the Python script with the Spotify API credentials
+echo "Updating the Python script with your Spotify API credentials..."
+sed -i "s/XXXXXXXXXXXXXXXXXXXXXX/$client_id/g" SpotifyAlbumArt.py/HDMI_SpotifyAlbumArt.py
+sed -i "s/XXXXXXXXXXXXXXXXXXXXXXXXX/$client_secret/g" SpotifyAlbumArt.py/HDMI_SpotifyAlbumArt.py
+
+# Ask the user for the display output type
+attempt=0
+while [ $attempt -lt 3 ]; do
+    echo "Choose the display output type:"
+    echo "1. HDMI"
+    echo "2. LED Matrix"
+    read -p "Enter the number (1 or 2): " choice
+    
+    if [ "$choice" -eq 1 ]; then
+        echo "Running the HDMI_SpotifyAlbumArt.py script..."
+        python3 SpotifyAlbumArt.py/HDMI_SpotifyAlbumArt.py
+        break
+    elif [ "$choice" -eq 2 ]; then
+        echo "Running the LED_SpotifyAlbumArt.py script..."
+        python3 SpotifyAlbumArt.py/LED_SpotifyAlbumArt.py
+        break
+    else
+        echo "Invalid choice. Please try again."
+        ((attempt++))
+        if [ $attempt -eq 3 ]; then
+            echo "Too many invalid attempts. Exiting."
+            exit 1
+        fi
+    fi
+done
+
+echo "Installation completed successfully!"
